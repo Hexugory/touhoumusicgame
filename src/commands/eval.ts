cff7f63272d8c59fdf55acc19c1b7ad623302f95
Hexugory <jimmy.mccall4@gmail.com>
2023-04-01 13:19:43 -0500

    this is fine probably

---
+++ src/commands/eval.ts
@@ -0,0 +1,33 @@
+import { Message } from "discord.js"
+import { Command } from "./command"
+import { StringArgument } from "../types/string"
+
+interface EvalArguments {
+    code: string
+}
+
+export class EvalCommand implements Command {
+	name = 'eval'
+    aliases = []
+    description = 'that thing'
+    usage = 'yeah'
+    permission = []
+    guildOnly = false
+    ownerOnly = true
+    args = [
+        {
+            key: 'code',
+            type: StringArgument,
+            infinite: false,
+            optional: false
+        }
+    ]
+
+	async execute(msg: Message, arglist: {}) {
+        const args = (arglist as EvalArguments);
+        const result = eval(args.code);
+        console.log(args.code);
+        console.log(result);
+        return msg.channel.send(result.toString());
+	}
+};
